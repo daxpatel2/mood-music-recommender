3f11b5062482362e272bd35edc763d8f229203de
UnamrkedBanshee <dxp38@pct.edu>
2024-12-21 19:17:20 -0500

    init server logic

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const SpotifyStrategy = require("passport-spotify").Strategy;
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, // Allow cookies to be sent
+  })
+);
+
+// Session configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
